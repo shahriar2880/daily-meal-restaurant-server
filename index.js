299aa9f2270aef4ffdf404d8e79fcc8fc88f5e62
shahriar2880 <shahriarreza436@gmail.com>
2024-05-25 15:57:06 +0600

    user role

--- index.js
+++ index.js
@@ -50,6 +50,20 @@ async function run() {
       res.send(result);
     });
 
+    //change user role
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+    //delete user
     app.delete('/users/:id', async(req, res)=>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
