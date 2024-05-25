561290b418d36a8610895ef7708318edad57194e
shahriar2880 <shahriarreza436@gmail.com>
2024-05-25 15:29:53 +0600

    delete users route

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
     //menu Collection
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
