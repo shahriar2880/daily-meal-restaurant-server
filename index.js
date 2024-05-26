fd87abca88925005b59404f57ece132cb2b1d738
shahriar2880 <shahriarreza436@gmail.com>
2024-05-26 22:52:35 +0600

    jwt related api

--- index.js
+++ index.js
@@ -31,8 +31,13 @@ async function run() {
     const reviewCollection = client.db("resturent").collection("reviews");
     const cartCollection = client.db("resturent").collection("carts");
 
+// jwt related api
+app.post('/jwt', async(req, res)=>{
+  const user = req.body ;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'}) ;
+  res.send({token})
+})
 
-    
     //user related api
     app.get('/users', async(req, res) =>{
       const result = await userCollection.find().toArray();
