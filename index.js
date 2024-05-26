b5bb8a35a3cfc19e9464e5bf607e8090215d56eb
shahriar2880 <shahriarreza436@gmail.com>
2024-05-26 23:35:05 +0600

    middlewar verifyToken

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5050;
 
@@ -31,53 +31,70 @@ async function run() {
     const reviewCollection = client.db("resturent").collection("reviews");
     const cartCollection = client.db("resturent").collection("carts");
 
-// jwt related api
-app.post('/jwt', async(req, res)=>{
-  const user = req.body ;
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'}) ;
-  res.send({token})
-})
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbiden access'})
+        }
+        req.decoded = decoded ;
+        next();
+      })
+    };
 
     //user related api
-    app.get('/users', async(req, res) =>{
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
-
+    });
 
     //user api
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user dosen't exists
-      const query = {email: user.email}
-      const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedId : null})
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
       }
-      const result = await userCollection.insertOne(user); 
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
     //change user role
-    app.patch('/users/admin/:id', async(req, res)=>{
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          role: 'admin'
-        }
-      }
-      const result = await userCollection.updateOne(filter, updatedDoc)
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
-    })
+    });
 
     //delete user
-    app.delete('/users/:id', async(req, res)=>{
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     //menu Collection
     app.get("/menu", async (req, res) => {
@@ -99,7 +116,7 @@ app.post('/jwt', async(req, res)=>{
       res.send(result);
     });
 
-    // sent cart data to database 
+    // sent cart data to database
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
