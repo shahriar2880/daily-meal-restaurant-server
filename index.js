7b7e799f629ea34db379d00f2dc427bacd1d4345
shahriar2880 <shahriarreza436@gmail.com>
2024-05-29 03:04:10 +0600

    is admin route

--- index.js
+++ index.js
@@ -60,6 +60,22 @@ async function run() {
       res.send(result);
     });
 
+    // is admin?
+    app.get('/user/admin/:email', verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req.decode.email){
+        return res.status(403).send({message: 'unouthorized access'})
+      }
+
+      const query ={email:email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'
+      }
+      res.send({admin});
+    })
+
     //user api
     app.post('/users', async (req, res) => {
       const user = req.body;
