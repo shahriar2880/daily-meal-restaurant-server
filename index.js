712c08287ffb393a11afad4a7ff80f70639a5dbe
shahriar2880 <shahriarreza436@gmail.com>
2024-05-30 02:00:11 +0600

    use verify admin after verifyToken

--- index.js
+++ index.js
@@ -54,8 +54,21 @@ async function run() {
       })
     }
 
+    //use verify admin after verifyToken
+    const verifyAdmin = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query ={ email : email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin' ;
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next();
+
+    }
+
     // users related api
-    app.get('/users', verifyToken, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -92,7 +105,7 @@ async function run() {
     });
 
     //change user role
-    app.patch('/users/admin/:id', verifyToken, async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -105,7 +118,7 @@ async function run() {
     })
 
     //delete user
-    app.delete("/users/:id",verifyToken, async (req, res) => {
+    app.delete("/users/:id",verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
