99369e4a48e1913718c4452f4000312ebdfd07fe
shahriar2880 <shahriarreza436@gmail.com>
2024-06-07 23:13:00 +0600

    get menu by id

--- index.js
+++ index.js
@@ -137,6 +137,14 @@ async function run() {
       res.send(result);
     });
 
+     //------------------get menu by id------------------//
+    app.get('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     //------------------set menu into database------------------//
 
     app.post('/menu', verifyToken, verifyAdmin, async (req, res)=>{
