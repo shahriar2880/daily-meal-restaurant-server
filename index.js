98290df1bb670cd5c242a4138187a7ddc6d3d166
shahriar2880 <shahriarreza436@gmail.com>
2024-06-09 02:15:54 +0600

    Save Payment Info And Delete Cart Items In The Database

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     const menuCollection = client.db("resturent").collection("menu");
     const reviewCollection = client.db("resturent").collection("reviews");
     const cartCollection = client.db("resturent").collection("carts");
+    const paymentCollection = client.db("resturent").collection("payments");
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -243,6 +244,24 @@ async function run() {
         clientSecret: paymentIntent.client_secret,
       });
     });
+
+    //----------------------------payment related api--------------------------------//
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //  carefully delete each item from the cart
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+    })
 
     //------------------Send a ping to confirm a successful connection------------------//
 
