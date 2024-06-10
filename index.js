5ac445ae1920590c6234b2cc0c776c29b59db014
shahriar2880 <shahriarreza436@gmail.com>
2024-06-11 01:22:30 +0600

    Get Order Quantity And Revenue By Category

--- index.js
+++ index.js
@@ -306,6 +306,43 @@ async function run() {
       });
     });
 
+    //using aggregate pipeline
+    app.get('/order-stats', verifyToken, verifyAdmin, async(req, res) =>{
+      const result = await paymentCollection.aggregate([
+        {
+          $unwind: '$menuItemIds'
+        },
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItemIds',
+            foreignField: '_id',
+            as: 'menuItems'
+          }
+        },
+        {
+          $unwind: '$menuItems'
+        },
+        {
+          $group: {
+            _id: '$menuItems.category',
+            quantity:{ $sum: 1 },
+            revenue: { $sum: '$menuItems.price'} 
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            category: '$_id',
+            quantity: '$quantity',
+            revenue: '$revenue'
+          }
+        }
+      ]).toArray();
+      res.send(result);
+    })
+
+
     //------------------Send a ping to confirm a successful connection------------------//
 
     await client.db("admin").command({ ping: 1 });
